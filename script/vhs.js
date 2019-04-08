ea24f0a836d3579a182e70681c2d915a77dcbe93
Bikelund <nicklas_barklund@hotmail.com>
2019-04-08 20:43:12 +0200

    Strukuerade koden och filerna

--- script/vhs.js
+++ script/vhs.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
         let movieBox = "";
         let itemsArray = localStorage.getItem('Varukorgen') ? JSON.parse(localStorage.getItem('Varukorgen')) : []; // En array med objekt från LS (annars tom)
         let movies = response; // Array med alla objekt från JSON
-
         
         // ======================================== //
         // On Load
@@ -20,31 +19,12 @@ $(document).ready(function(){
             newList.innerHTML += movieBox;
         };
 
-        // Öppna Info-divvar
-
-        $(".infobutton").on("click", function(event){
-            let movieID = event.target.id;
-            movieID = movieID.replace("movie",""); // Ta bort "movie" ur IDt för att hitta index
-            
-            $(".info#info"+[movieID]+"").fadeIn();
-            $(".info#info"+[movieID]+"").append("<img src='" + movies[movieID].cover + "'><p>" + movies[movieID].description + 
-            "<br><br>Årtal: " + movies[movieID].year + "<br>Speltid: " + movies[movieID].runtime + 
-            "<br>Regissör: " + movies[movieID].director + "<br>Pris: " + movies[movieID].price + " kr<br>" +
-            "Genre: " + movies[movieID].genre + "</p>");
-        });
-
-        // Stänger info-divvar
-        $(".overlay-close").on("click", function(){
-            $(".info").hide();
-            $(".info").find("*").not("span.overlay-close").empty();
-        })
-        
         // Skriver ut innehållet i varukorgen när sidan laddas
         for (let i = 0; i < itemsArray.length; i++) {
             basketList.append("<li id='movieLi" + itemsArray[i].id + "'>" + itemsArray[i].title + " x <span id='quantity" + itemsArray[i].id + "'>" + localStorage.getItem("Quantity" + itemsArray[i].id) + "</span><button class='remove'>X</button></li>");
         };
 
-        counterCheck();
+        counterCheck(); // Kollar och skriver ut antal varor i varukorgen
 
         // ======================================= //
         // Klickfunktioner
@@ -56,6 +36,7 @@ $(document).ready(function(){
 
             let quantity = document.getElementById("quantity" + movies[movieID].id) ? document.getElementById("quantity" + movies[movieID].id) : 0;
 
+            // Deklarerar antal av samma film
             let qaParse = 1;
 
             if (quantity == 0 ){
@@ -69,30 +50,55 @@ $(document).ready(function(){
 
                 localStorage.setItem("Quantity" + movies[movieID].id, qaParse);
     
-                
             }
             else {
                 qaParse = parseInt(quantity.innerHTML, 10);
                 qaParse++;
+                
                 localStorage.setItem("Quantity" + movies[movieID].id, qaParse);
                 quantity.innerHTML = qaParse;
             }
+
+            // Grön bekräftelse när man klicka på köpknapp
+            event.target.style.backgroundColor = "green";
+            setTimeout(function(){
+                event.target.style.backgroundColor = "#D23B26";
+            }, 500);
+
             counterCheck();
+        });
+
+        // Öppna info-rutan vid klick på bild
+        $(".infobutton").on("click", function(event){
+            let movieID = event.target.id;
+            movieID = movieID.replace("movie","");
+            
+            $(".info#info"+[movieID]+"").fadeIn();
+            $(".info#info"+[movieID]+"").append("<img src='" + movies[movieID].cover + "'><p>" + movies[movieID].description + 
+            "<br><br>Årtal: " + movies[movieID].year + "<br>Speltid: " + movies[movieID].runtime + 
+            "<br>Regissör: " + movies[movieID].director + "<br>Pris: " + movies[movieID].price + " kr<br>" +
+            "Genre: " + movies[movieID].genre + "</p>");
+        });
+
+        // Stänger info-rutan
+        $(".overlay-close").on("click", function(){
+            $(".info").hide();
+            $(".info").find("*").not("span.overlay-close").empty();
         });
 
         // Rensar valfritt objekt ur varukorgen
         $('#basketList').on("click", function(event){
             
             if ($(event.target).hasClass("remove")) {
-                // Removes list element for cart
+                // Tar bort list-elementet från varukorgen
                 $(event.target.parentElement).remove();
             }
 
-            // Gets an ID number from li id
+            // Tar id från ul li
             let movieID = event.target.parentElement.id;
             movieID = movieID.replace("movieLi", "");
 
-            // Removes object from LS
+            // Tar bort objektet från LS
             $.each(itemsArray, function(i){
                 if(itemsArray[i].id == movieID) {
                     itemsArray.splice(i,1); // Removes object from index position
@@ -114,19 +120,14 @@ $(document).ready(function(){
         // ======================================= //
         // Functions
 
+        // Kollar vilka varor som finns i LS och skriver ut antalet i varukorgen
         function counterCheck(){
-
             let antalVaror = 0;
             $(itemsArray).each(function(i,movie){
-      
-              let antal = parseInt(localStorage.getItem('Quantity' + movie.id));
-              antalVaror += antal;
-      
+                let antal = parseInt(localStorage.getItem('Quantity' + movie.id));
+                antalVaror += antal;
             });
-            
-            counter.innerHTML = antalVaror;
-            
+            counter.innerHTML = antalVaror; 
         };
-
     });  
 });
