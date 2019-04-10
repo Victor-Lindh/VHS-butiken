63c50e326a80e6addd9c7ddd476004dadbcf11bd
Bikelund <nicklas_barklund@hotmail.com>
2019-04-10 17:11:21 +0200

    Major code cleanup and bug fix

--- script/betalsida.js
+++ script/betalsida.js
@@ -16,7 +16,6 @@
   }, false)
 }())
   
-  
 $(document).ready(function(){
   
   //filmer i LS
@@ -26,17 +25,13 @@ $(document).ready(function(){
 
   //Itemsarray.length blir siffran i elementet med id="counter"
   function counterTest(){
-    let counter = document.getElementById('counter2');
     let antalVaror = 0;
-    $(itemsArray).each(function(i,movie){
-
-      let antal = parseInt(localStorage.getItem('Quantity' + movie.id));
-      antalVaror += antal;
-
-    });
-    
-    counter.innerHTML = antalVaror;
-    
+    for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage.key(i) != "Varukorgen"){
+        antalVaror += parseInt(localStorage.getItem(localStorage.key(i)));
+      }
+    }
+    document.getElementById('counter2').innerHTML = antalVaror;  
   };
 
   varukorg();
@@ -45,137 +40,118 @@ $(document).ready(function(){
     
     let items = $('#items');
     let totalPrice = 0;
-    //Loopar igenom LS för att hämta innehållet
 
     //loopar igenom arrayen
     $(itemsArray).each(function(i, movie){
 
       //hämtar quantity och kopplar till rätt film.
-      let antal = localStorage.getItem('Quantity' + movie.id);
+      let antal = localStorage.getItem('Quantity' + (movie.id - 1));
 
       //lägger till varje films pris i totalPrice variabeln
       let moviePrice = parseInt(movie.price);
       let antalPris = moviePrice * antal;
-      console.log(antalPris)
       totalPrice += antalPris;
 
-
       //Lägger till values från itemsArray som produkter i id="items"
-      $(items).append(`
-      <li class="list-group-item d-flex justify-content-between lh-condensed">
-                <div>
-                  <h6 class="my-0">${movie.title}</h6>
-                  <small class="descrip">Antal: ${antal}</small>
-                </div>
-                <span class="descrip">${antalPris}Kr</span>
-              </li>
-      `);
+      $(items).append(
+        `<li class="list-group-item d-flex justify-content-between lh-condensed">
+          <div>
+            <h6 class="my-0">${movie.title}</h6>
+            <small class="descrip">Antal: ${antal}</small>
+          </div>
+          <span class="descrip">${antalPris}Kr</span>
+        </li>`
+      );
     });
 
-  $(items).append(`
-  <li class="list-group-item d-flex justify-content-between">
-              <span>Summa</span>
-              <strong>${totalPrice}Kr</strong>
-            </li>
-` );
-};
+    $(items).append(
+      `<li class="list-group-item d-flex justify-content-between">
+        <span>Summa</span>
+        <strong>${totalPrice}Kr</strong>
+      </li>`
+    );
+  };
 
   $('#subBtn').click(function(){
-    //deklarerar värden för inputfälten
-  let firstName = $('#firstName').val();
-  let lastName = $('#lastName').val();
-  let email = $('#email').val();
-  let country = $('#country').val();
-  let adress = $('#adress').val();
-  let city = $('#stad').val();
-  let zip = $('#zip').val();
-
-    //lägger de sedan i LS
-  localStorage.setItem('firstName', firstName);
-  localStorage.setItem('lastName', lastName);
-  localStorage.setItem('email', email);
-  localStorage.setItem('adress', adress)
-  localStorage.setItem('country', country);
-  localStorage.setItem('city', city)
-  localStorage.setItem('zip', zip);
-
+    // Lägger till värdet från varje input-fält i LS
+    localStorage.setItem('firstName', $('#firstName').val());
+    localStorage.setItem('lastName', $('#lastName').val());
+    localStorage.setItem('email', $('#email').val());
+    localStorage.setItem('adress', $('#adress').val())
+    localStorage.setItem('country', $('#country').val());
+    localStorage.setItem('city', $('#stad').val())
+    localStorage.setItem('zip', $('#zip').val());
   });
 
-
-    
   let firstName = localStorage.getItem('firstName');
-  let lastName = localStorage.getItem('lastName');
-  let email = localStorage.getItem('email');
-  let adress = localStorage.getItem('adress')
-  let country = localStorage.getItem('country');
-  let city = localStorage.getItem('city');
-  let zip = localStorage.getItem('zip');
+  let lastName  = localStorage.getItem('lastName');
+  let email     = localStorage.getItem('email');
+  let adress    = localStorage.getItem('adress')
+  let country   = localStorage.getItem('country');
+  let city      = localStorage.getItem('city');
+  let zip       = localStorage.getItem('zip');
 
   $('#text').html(`
-  <h2 class="d-flex justify-content-center my-3"> Tack ${firstName} ${lastName} för ditt köp! </h2>
+  <h2 class="d-flex justify-content-center my-3"> Tack ${firstName} för ditt köp!</h2>
   <p> Vi uppskattar att du valt att handla hos oss. Dina varor kommer bli levererade av DBschenker till närmsta ombud
-  för <strong>${adress}</strong>. </p>
+  för:</p>
+  <div id='shippingAddress'>
+  ${firstName} ${lastName}<br>
+  ${adress}<br>
+  ${zip} ${city}</div>
   <p> Så snart din varor är packade kommer du få ett meddelande skickat till <strong>${email}</strong> om att ditt paket är på väg till dig.</p>
-  <p> Vänliga hälsningar från oss på Play VHS. </p>
-  
+  <p> Vänliga hälsningar från oss på Play VHS.</p>
   `);
 
   receiptTable();
 
   function receiptTable(){
     
-      let rows = $('#receiptBody');
-      let totalPrice = 0;
-      let varunummer = 0;
+    let rows = $('#receiptBody');
+    let totalPrice = 0;
+    let varunummer = 0;
 
-      //loopar igenom arrayen
+    //loopar igenom arrayen
     $(itemsArray).each(function(i, movie){
 
     //hämtar quantity och kopplar till rätt film.
-    let antal = localStorage.getItem('Quantity' + movie.id);
+    let antal = localStorage.getItem('Quantity' + (movie.id - 1));
 
     //lägger till varje films pris i totalPrice variabeln
     let moviePrice = parseInt(movie.price);
     let antalPris = moviePrice * antal;
-    console.log(antalPris)
     totalPrice += antalPris;
     varunummer++;
 
     //Lägger till values från itemsArray som produkter i id="items"
     $(rows).append(`
     <tr>
-    <th scope="row">${varunummer}</th>
-    <td>${movie.title}</td>
-    <td>${movie.director}</td>
-    <td>${antal}</td>
-    <td>${antalPris} Kr</td>
-  </tr>
+      <th scope="row">${varunummer}</th>
+      <td>${movie.title}</td>
+      <td>${movie.director}</td>
+      <td>${antal}</td>
+      <td>${antalPris} Kr</td>
+    </tr>
     `);
   });
   
   $(rows).append(`
-
   <tr>
-  <td>&nbsp;</td>
-  <td>&nbsp;</td>
-  <td>&nbsp;</td>
-  <td class="text-right">
-  <h6> <strong>Totalsumma: </strong></h6>
-  </td>
-  <td class="text-center text-danger"> <h6><strong>${totalPrice}Kr</strong></h6> </td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td class="text-right">
+    <h6> <strong>Totalsumma: </strong></h6>
+    </td>
+    <td class="text-center text-danger"> <h6><strong>${totalPrice}Kr</strong></h6> </td>
   </tr>
   `);
-      
-  
-  };//Receipttable()
+  };
 
   $('#knappN').click(function(){
-
-      localStorage.clear();
-      itemsArray = [];
-      basketList.empty();
-      
+    localStorage.clear();
+    itemsArray = [];
+    basketList.empty();
   });
-
-}); //Ready
+}); // Ready
   
